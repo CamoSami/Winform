993dce41489f37d067d01560bbdb00a71fcf869d
Thanhtruc0803 <Buithanhtruc0608@gmail.com>
2023-10-26 09:07:48 +0700

    DTONhanVien, DTOCongViec

---
+++ WinformWithExternalLibrary/DataTransferObjects/CongViecDTO.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinformWithExternalLibrary.DataTransferObjects
+{
+    public class CongViecDTO
+    {
+        [Required(ErrorMessage = "Thiếu mã công việc!")]
+        [DisplayName("Mã công việc")]
+        public int CongViecDTO_MaCongViec { get; set; }
+
+        [Required(ErrorMessage = "Thiếu mức lương!")]
+        [DisplayName("Mức lương")]
+        public decimal CongViecDTO_MucLuong { get; set; }
+
+        [Required(ErrorMessage = "Thiếu tên công việc!")]
+        [StringLength(maximumLength: 40,
+                        MinimumLength = 4,
+                        ErrorMessage = "Nhập tên công việc từ 4-40 kí tự.")]
+        [DisplayName("Tên công việc")]
+        public string CongViecDTO_TenCongViec { get; set; }
+
+        public CongViecDTO(int maCongViec, decimal mucLuong, string tenCongViec)
+        {
+            this.CongViecDTO_MaCongViec = maCongViec;
+            this.CongViecDTO_MucLuong = mucLuong;
+            this.CongViecDTO_TenCongViec = tenCongViec;
+        }
+
+        public CongViecDTO() { }
+    }
+}
---
+++ WinformWithExternalLibrary/DataTransferObjects/NhanVienDTO.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinformWithExternalLibrary.DataTransferObjects
+{
+    public class NhanVienDTO
+    {
+        [Required(ErrorMessage = "Thiếu mã nhân viên!")]
+        [DisplayName("Mã nhân viên")]
+        public int NhanVienDTO_MaNhanVien { get; set; }
+
+        [Required(ErrorMessage = "Thiếu mã công việc!")]
+        [DisplayName("Mã công việc")]
+        public int NhanVienDTO_MaCongViec { get; set; }
+
+        [Required(ErrorMessage = "Thiếu tên nhân viên!")]
+        [StringLength(maximumLength: 40,
+                       MinimumLength = 4,
+                    ErrorMessage = "Nhập tên nhân viên từ 4-40 kí tự.")]
+        [DisplayName("Tên nhân viên")]
+        public string NhanVienDTO_TenNhanVien { get; set; }
+
+        [Required(ErrorMessage = "Thiếu ngày sinh!")]
+        [DisplayName("Ngày sinh")]
+        [DataType(DataType.Date)]
+        public DateTime NhanVienDTO_NgaySinh { get; set; }
+
+        [Required(ErrorMessage = "Thiếu địa chỉ!")]
+        [StringLength(maximumLength: 100,
+                        MinimumLength = 4,
+            ErrorMessage = "Nhập địa chỉ từ 4-100 kí tự.")]
+        [DisplayName("Địa chỉ")]
+        public string NhanVienDTO_DiaChi { get; set; }
+
+        [Required(ErrorMessage = "Thiếu số điện thoại!")]
+        [StringLength(  maximumLength: 10,
+                        MinimumLength = 10,
+                        ErrorMessage = "Nhập số điện thoại 10 kí tự.")]
+        [DisplayName("Số điện thoại")]
+        public string NhanVienDTO_DienThoai { get; set; }
+
+        [Required(ErrorMessage = "Thiếu giới tính!")]
+        [DisplayName("Giới tính")]
+        public string NhanVienDTO_GioiTinh { get; set; }
+
+        public NhanVienDTO(int maNhanVien, int maCongViec, string tenNhanVien, DateTime ngaySinh, string diaChi, string dienThoai, string gioiTinh)
+        {
+            this.NhanVienDTO_MaNhanVien = maNhanVien;
+            this.NhanVienDTO_MaCongViec = maCongViec;
+            this.NhanVienDTO_TenNhanVien = tenNhanVien;
+            this.NhanVienDTO_NgaySinh = ngaySinh;
+            this.NhanVienDTO_DiaChi = diaChi;
+            this.NhanVienDTO_DienThoai = dienThoai;
+            this.NhanVienDTO_GioiTinh = gioiTinh;
+        }
+
+        public NhanVienDTO() { }
+    }
+}
